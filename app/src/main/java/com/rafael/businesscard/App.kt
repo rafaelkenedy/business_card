b57f2155c603b8ab98c39fe0678e150c21f00757
rafaelkenedy <k.rafaelalves@gmail.com>
2022-07-04 00:47:59 -0300

    Setting entities and database

--- app/src/main/java/com/rafael/businesscard/App.kt
+++ app/src/main/java/com/rafael/businesscard/App.kt
@@ -1,6 +1,11 @@
 package com.rafael.businesscard
 
 import android.app.Application
+import com.rafael.businesscard.data.AppDatabase
+import com.rafael.businesscard.data.BusinessCardRepository
 
 class App : Application() {
+
+    val database by lazy { AppDatabase.getDatabase(this) }
+    val repository by lazy { BusinessCardRepository(database.businessDao()) }
 }
