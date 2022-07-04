5679e95e81573d54af57609bc5c3a221a80170c3
rafaelkenedy <k.rafaelalves@gmail.com>
2022-07-04 02:21:19 -0300

    Adding ui functions

--- app/src/main/java/com/rafael/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/com/rafael/businesscard/ui/AddBusinessCardActivity.kt
@@ -3,11 +3,19 @@ package com.rafael.businesscard.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.SyncStateContract.Helpers.insert
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.rafael.businesscard.App
+import com.rafael.businesscard.R
+import com.rafael.businesscard.data.BusinessCard
 import com.rafael.businesscard.databinding.ActivityAddBusinessCardBinding
 
 class AddBusinessCardActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater)  }
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +29,17 @@ class AddBusinessCardActivity : AppCompatActivity() {
         }
 
         binding.btnConfirmar.setOnClickListener{
+            val businessCard = BusinessCard(
+                nome = binding.tilNome.editText?.text.toString(),
+                empresa = binding.tilEmpresa.editText?.text.toString(),
+                telefone = binding.tilTelefone.editText?.text.toString(),
+                email = binding.tilEmail.editText?.text.toString(),
+                fundoPersonalizado = binding.tilCor.editText?.text.toString(),
+            )
 
+            mainViewModel.insert(businessCard)
+            Toast.makeText(this, R.string.label_show_success, Toast.LENGTH_SHORT).show()
+            finish()
         }
     }
 }
--- app/src/main/java/com/rafael/businesscard/ui/MainActivity.kt
+++ app/src/main/java/com/rafael/businesscard/ui/MainActivity.kt
@@ -1,25 +1,40 @@
 package com.rafael.businesscard.ui
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.rafael.businesscard.App
 import com.rafael.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+
+    private val adapter by lazy { BusinessCardAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.rvCards.adapter = adapter
+        getAllBusinessCard()
         insertListener()
     }
 
 
     private fun insertListener() {
-        binding.fab.setOnClickListener{
+        binding.fab.setOnClickListener {
             val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun getAllBusinessCard() {
+        mainViewModel.getAll().observe(this, {businessCards ->
+            adapter.submitList(businessCards)
+        })
     }
 }
